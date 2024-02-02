d0f1cada238e0170c01005ace024b25f777e91b1
deryu_mw14 <deryu_mw14@top-academy.ru>
2024-02-02 14:05:02 +0300

    HomeWork

---
+++ Array/main.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#define TAB "\t"
+#define ENTERARRAY
+int main()
+{
+#if defined ENTERARRAY
+std::cout << "Enter Array's size:" << TAB;
+int sz = 0;
+std::cin >> sz;
+std::cout << "Enter array elements from Keyboard: " << std::endl;
+int array[sz];
+int * count = new int;
+for(int &i: array)
+{
+    std::cout << ++*count << " element" << TAB << ": ";
+    std::cin >> i;
+}
+std::cout << std::endl;
+std::cout << "Elements of your Array" << std::endl;
+delete count;
+count = nullptr;
+count = new int(0);
+for(const int &i: array)
+{
+    std::cout << ++*count << " element is: " << TAB << i << std::endl;
+}
+delete count;
+count = nullptr;
+std::cout << std::endl;
+std::cout << "Array elements in reverse order" << std::endl;
+for(int i = sz - 1; i >= 0; i--)
+{
+        std::cout << i + 1 << " element is: " << TAB << array[i] << std::endl;   
+}
+std::cout << std::endl;
+std::cout << "Sum array's elements: "; 
+count = new int(0);
+for(const int &i: array)
+{
+    *count += i; 
+}
+std::cout << TAB << *count << std::endl;
+std::cout << std::endl;
+std::cout << "Average arra's elements value: " << TAB << *count / sz << std::endl;
+std::cout << std::endl;
+delete count;
+count = nullptr;
+
+std::cout << "Min and Max values in the Array" << std::endl;
+int min = array[0];
+int max = 0;
+for(const int &i : array)
+{
+    if(max < i)
+        max = i;
+    if(min >= i)
+        min = i;
+}
+std::cout << "Max is: " << TAB << max << std::endl;
+std::cout << "Min is: " << TAB << min << std::endl;
+std::cout << std::endl;
+#endif
+    return 0;
+}
